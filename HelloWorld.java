f75afeb8ffaca7cf976010ef8cdc81c4331166f4
alexandrun2 <33052149+alexandrun2@users.noreply.github.com>
2022-08-09 12:37:03 +0300

    test
    test

--- HelloWorld.java
+++ HelloWorld.java
@@ -21,7 +21,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         // Prints "Hello, World" in the terminal window.
-        System.out.println("Hello, World Z from GHG. Xoooo!");
+        System.out.println("Hello, World Z from GHG. Xoooo! Updateeeeed");
     }
 
 }
